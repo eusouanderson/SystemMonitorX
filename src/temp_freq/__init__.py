4f837106bf4278421f66a0d7714f7587f3cd70f1
Anderson B.O.B <55605767+eusouanderson@users.noreply.github.com>
2023-06-11 08:10:17 -0300

    "update"

--- src/temp_freq/__init__.py
+++ src/temp_freq/__init__.py
@@ -3,11 +3,15 @@ import psutil
 
 def temp():
     sensor = psutil.sensors_temperatures()
-    acpitz = sensor['acpitz'][1][1]
-
+    acpitz = sensor['coretemp'][0][1]
     temp = '{:.2f}'.format(acpitz)
+    print(temp)
     return temp
 
+
+
+#psutil = {'acpitz': [shwtemp(label='', current=6.8, high=18.8, critical=18.8), shwtemp(label='', current=27.8, high=119.0, critical=119.0), shwtemp(label='', current=29.8, high=119.0, critical=119.0)]
+#          , 'coretemp': [shwtemp(label='Package id 0', current=45.0, high=80.0, critical=100.0), shwtemp(label='Core 0', current=45.0, high=80.0, critical=100.0), shwtemp(label='Core 1', current=45.0, high=80.0, critical=100.0), shwtemp(label='Core 2', current=43.0, high=80.0, critical=100.0), shwtemp(label='Core 3', current=44.0, high=80.0, critical=100.0)],
+#            'nouveau': [shwtemp(label='', current=43.0, high=95.0, critical=130.0)], 'amdgpu': [shwtemp(label='edge', current=32.0, high=94.0, critical=94.0)]}
+
 temp()
-
-
