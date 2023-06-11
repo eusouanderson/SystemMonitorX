e6e6f4174888443355c519463d08dbda3c924efb
Anderson B.O.B <55605767+eusouanderson@users.noreply.github.com>
2023-06-11 08:17:39 -0300

    "update"

--- src/module.py
+++ src/module.py
@@ -12,15 +12,15 @@ app.wm_overrideredirect(True)
 
 def update(time=1000):
     
-    label_cpu.config(text=cpu())
-    label_temp.config(text=temp())
+    label_cpu.config(text=f'{cpu()} HZ ')
+    label_temp.config(text=f'{temp()} Cº ')
     app.after(time, update)
 
 
-label_cpu = tk.Label(app, text=cpu)
+label_cpu = tk.Label(app, text=f'{cpu} HZ ')
 label_cpu.pack()
 
-label_temp = tk.Label(app, text=temp)
+label_temp = tk.Label(app, text=f'{temp} Cº ')
 label_temp.pack()
 
 update(100)
--- src/temp_freq/__init__.py
+++ src/temp_freq/__init__.py
@@ -5,10 +5,7 @@ def temp():
     sensor = psutil.sensors_temperatures()
     acpitz = sensor['coretemp'][0][1]
     temp = '{:.2f}'.format(acpitz)
-    print(temp)
     return temp
-
-
 
 #psutil = {'acpitz': [shwtemp(label='', current=6.8, high=18.8, critical=18.8), shwtemp(label='', current=27.8, high=119.0, critical=119.0), shwtemp(label='', current=29.8, high=119.0, critical=119.0)]
 #          , 'coretemp': [shwtemp(label='Package id 0', current=45.0, high=80.0, critical=100.0), shwtemp(label='Core 0', current=45.0, high=80.0, critical=100.0), shwtemp(label='Core 1', current=45.0, high=80.0, critical=100.0), shwtemp(label='Core 2', current=43.0, high=80.0, critical=100.0), shwtemp(label='Core 3', current=44.0, high=80.0, critical=100.0)],
