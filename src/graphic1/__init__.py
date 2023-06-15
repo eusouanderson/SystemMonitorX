757b3cebbe0348724f46ff346f2873394b5094f4
Anderson B.O.B <55605767+eusouanderson@users.noreply.github.com>
2023-06-15 14:30:42 -0300

    "update"

--- src/graphic1/__init__.py
+++ src/graphic1/__init__.py
@@ -1,40 +1,73 @@
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+from matplotlib import animation
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from random import randint
+import tkinter as tk
+import psutil
+
 
 
 def graph(window):
- 
+
+    bar1 = 0
+    bar2 = 0
+    bar3 = 0
+    bar4 = 0
+    bar5 = 0
+
     # Configurações iniciais
-    
     categorias = ['CPU ', 'RAM ', 'DISK', 'CORE ', 'NET ']
-    valores = [100, 0, 0, 0, 0]
-   
+    valores = [bar1, bar2, bar3, bar4, bar5]
+    
+    # Criar listas vazias para armazenar os valores de tempo e utilização da CPU
+
+
+   # Configurando gráfico
     fig, ax = plt.subplots(figsize=(6, 3))
     bar_containers = ax.bar(categorias, valores)
+    ax.set_ylim(0, 600)
+    #ax.set_xlabel()
+    #ax.set_ylabel()
 
-    # Função de atualização
-    def update(frame):
-        
-        for i, container in enumerate(bar_containers):
-            
-            if i == 0 or 1 or 2 or 3 or 4:
-                container.set_height(randint(0, 10 % 100))
-                
-                if 100 >= 6000 :
-                    container.set_color('red')
-                else:
-                    container.set_color('blue')
-
-
-    # Criação da animação
-    ani = FuncAnimation(fig, update, frames=range(10), interval=100, cache_frame_data=False)
-    
-
-    # Exibição do gráfico em tempo real
+    # Criar um widget de tela de desenho do Matplotlib
     canvas = FigureCanvasTkAgg(fig, master=window)
     canvas.draw()
     canvas.get_tk_widget().pack()
 
-plt.close()
+
+    # Função de atualização
+    def update(i):
+        nonlocal bar_containers
+
+        # Obter o valor da frequência da CPU usando o psutil
+        cpu_freq = psutil.cpu_freq().current
+        mem_percnt = psutil.virtual_memory().percent
+
+        # Atualizar o valor da barra correspondente
+        bar_containers[0].set_height(cpu_freq - 4000)
+        bar_containers[1].set_height(mem_percnt)
+        # Redesenhar o gráfico
+        ax.relim()
+        ax.autoscale_view()
+        canvas.draw()
+
+    # Criar uma janela Tkinter
+    
+
+    # Atualizar o gráfico a cada segundo
+    anim = animation.FuncAnimation(fig, update, interval=1000)
+
+    def start_animation():
+        anim.event_source.start()
+
+    def stop_animation():
+        anim.event_source.stop()
+
+    # Adicionar botões para iniciar e parar a animação
+    start_button = tk.Button(window, text="Start", command=start_animation)
+    start_button.pack()
+
+    stop_button = tk.Button(window, text="Stop", command=stop_animation)
+    stop_button.pack()
+
+    
+
